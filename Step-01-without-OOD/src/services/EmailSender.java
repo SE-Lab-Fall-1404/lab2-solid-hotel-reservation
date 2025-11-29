be6a75ec512d7681ac878567de5481220408dce6
HamedSY <hamed007.saboor@gmail.com>
2025-11-29 16:07:25 +0330

    feat: add send sms method to email sender

--- Step-01-without-OOD/src/services/EmailSender.java
+++ Step-01-without-OOD/src/services/EmailSender.java
@@ -4,4 +4,8 @@ class EmailSender implements MessageSender{
     public void sendEmail(String to, String message){
         System.out.println("Sending email to " + to + ": " + message);
     }
+    
+    public void sendSmsMessage(String to, String message){
+        // EmailSender doesn't send SMS
+    }
 }
